1e1904509663ab8b2eb39249a5d1994ac603f128
yutao <ytxinguan035@163.com>
2018-12-06 22:11:47 +0800

    init

---
+++ VueDemos/FirstVue/src/components/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="hello">
+    <div>FirrstDemo</div>
+    <section>
+      <p>
+        1.路由router 
+        2.网络请求
+        3.初始化JS
+        4.elementUI
+        5.Vuex 存储
+        6.开发
+        7.适配移动端
+        8.界面UICS 
+      </p>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+       
+    }
+  }
+}
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+ 
+</style>
