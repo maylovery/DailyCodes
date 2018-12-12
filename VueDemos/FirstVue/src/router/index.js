4317cfc723b98c14f6c274abc8936328fb7d8c56
yutao <ytxinguan035@163.com>
2018-12-12 16:05:55 +0800

    change

--- VueDemos/FirstVue/src/router/index.js
+++ VueDemos/FirstVue/src/router/index.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '@/components/Home'
-import Test from '@/common/Test'
+
+import Home from '@/pages/Home'
+import Test from '@/pages/Test'
+
 Vue.use(Router)
 
 export default new Router({
